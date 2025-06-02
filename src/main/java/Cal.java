9751be5ec80f75ef0b62cb5928687f5e1dd5f816
ariari01 <juh.shin@gmail.com>
2025-06-02 15:41:22 +0900

    getDivide

--- src/main/java/Cal.java
+++ src/main/java/Cal.java
@@ -1,5 +1,5 @@
 public class Cal {
-    public double getDivide(int a, int b) {
+    public int getDivide(int a, int b) {
         return a / b;
     }
 }
--- src/test/java/CalTest.java
+++ src/test/java/CalTest.java
@@ -1,10 +1,10 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalTest {
 
     @Test
-    void testDivide() {
+    void getDivide() {
         Cal cal = new Cal();
         assertEquals(2, cal.getDivide(10, 5));
     }
